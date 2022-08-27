4653c27ebe02c8e82632836a2e00fe0840797dba
IgnacioGhibaudo <ignacioaghibaudo@gmail.com>
2022-08-26 21:24:30 -0300

    Initial structure done, ready to develop.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from 'react-router';
+
+function App(){ 
+    return(
+        <Switch>
+            <Route />
+        </Switch>
+    )
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.listen(3080, () => {
+    console.log("Servidor levantado con éxito en el puerto 3080")
+})
